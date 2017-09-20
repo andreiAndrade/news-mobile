d0ad4ee592a89419a38951081013a563611e621a
Andrei Andrade <andrei.richard@totvs.com.br>
2017-09-20 15:57:40 -0300

    Ajustando tela de cadastro.

--- app/assets/styles/colors.tsx
+++ app/assets/styles/colors.tsx
@@ -2,6 +2,9 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 
 export default EStyleSheet.create({
     primaryColor: '#88B04B',
+    secondaryColor: '#2d607c',
+    primaryHighlightColor: '#f09d06',
+    secondaryHighlightColor: '#fdba1a',
     lightColor: '#ffffff',
     darkColor: '#000000'
 })
--- app/screens/Welcome/Welcome.tsx
+++ app/screens/Welcome/Welcome.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { View, ImageBackground } from 'react-native'
 import { Button } from 'react-native-elements'
+// import { colors } from '../../assets/styles'
 
-export default () => (
+export default ({navigation}) => (
   <ImageBackground style={{
     flex: 1,
     width: null,
     height: null,
-    flexWrap: 'wrap',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    flexDirection: 'row',
+    alignItems: 'flex-end'
     }} source={{uri: 'https://i.pinimg.com/736x/3c/2b/73/3c2b730d9d7e3a9ce07c252ae902eefb--starry-night-sky-night-skies.jpg'}}>
-    <View style={{}}>
-      <Button raised large title='Sign Up' style={{marginBottom: 20}} />
-      <Button raised large title='Sign In' />
+    <View style={{flex: 1, marginBottom: 50}}>
+      <Button raised title='Sign Up' style={{marginBottom: 20}} backgroundColor={'#88B04B'} onPress={() => navigation.navigate('SignUp')}/>
+      <Button raised title='Sign In' backgroundColor={'#2d607c'} onPress={() => navigation.navigate('SignIn')}/>
     </View>
   </ImageBackground>
 )
